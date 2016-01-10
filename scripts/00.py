f6b8fa1fbc99e2527b56d6f986f2fffdf258b291
hausen6 <hausen62480@gmail.com>
2016-01-11 01:07:24 +0900

    標準的なものを追加

---
+++ scripts/00.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals, division
+import os
+import sys
+from glob import glob
+
+try:
+    import numpy as np
+except (ImportError) as e:
+    print(e)
+try:
+    import matplotlib.pyplot as plt
+except (ImportError) as e:
+    print(e)
+try:
+    import seaborn as sns
+except (ImportError) as e:
+    print(e)
